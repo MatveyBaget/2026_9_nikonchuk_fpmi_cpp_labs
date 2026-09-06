632bb54e7d34caa1a91c33f771fa96e862f45a72
MatveyBaget <matvey30112008@gmail.com>
2026-09-06 16:49:41 +0300

    Update main.cpp

--- semester_1/lab1_introduction/main.cpp
+++ semester_1/lab1_introduction/main.cpp
@@ -1,35 +1,26 @@
 #include <iostream>
-#include <numeric> 
-#include <windows.h> 
-
+#include <windows.h>
+#include <algorithm>
 int main() {
-   
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+	int n, m;
+	std::cout << "ВЕДИТЕ( ЛЮБОЕ НАТУРАЛЬНОЕ ЧИСЛО n:";
+	std::cin >> n;
+	std::cout << "ведите любое натуральное число m:";
+	std::cin >> m;
+	if (m <= 0 || n <= 0) {
+		std::cout << "это не натурально число ведите другие";
+		return 0;
+	}
+	std::cout << "Общие делители чисел " << n << " и " << m << ": ";
+	int Max_number = (std::min)(n, m);
+	for (int i = 1; i <= Max_number; ++i) { 
+		if (n % i == 0 && m % i == 0) {
+			std::cout << i << " ";
+		}
+	}
+	std::cout << std::endl;
+	return 0;
 
-   int n, m;
-
-    std::cout << "Введите первое натуральное число (n): ";
-    std::cin >> n;
-
-    std::cout << "Введите второе натуральное число (m): ";
-    std::cin >> m;
-
-    if (n == 0 || m == 0) {
-        std::cout << "Числа должны быть натуральными (больше 0)." << std::endl;
-        return 1;
-    }
-
-  int gcd_value = std::gcd(n, m);
-
-    std::cout << "Общие делители чисел " << n << " и " << m << ":" << std::endl;
-
-    for ( int i = 1; i <= gcd_value; ++i) {
-        if (gcd_value % i == 0) {
-            std::cout << i << " ";
-        }
-    }
-    std::cout << std::endl;
-
-    return 0;
 }
